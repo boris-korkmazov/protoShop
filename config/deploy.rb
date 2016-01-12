fad97a879618c146a5d23e8a66f93772b9211219
boriz <boriskorkmazov1989@gmail.com>
2016-01-12 10:58:08 +0300

    Add header

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :deploy_to, '/opt/protoShop'
 
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', '.env')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', '.env', )
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
@@ -45,3 +45,4 @@ namespace :deploy do
 
 end
 after 'deploy:publishing', 'deploy:restart'
+after 'deploy:restart', 'deploy:seed'
